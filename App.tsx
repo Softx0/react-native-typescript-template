94bad8077f3970d0c23f7b8ecf16ab846cf67a8d
Emma <ing.valen0421@hotmail.com>
2024-10-06 18:27:15 -0400

    feat: trying dsdsd

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ function Section({ children, title }: SectionProps): React.JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {`${title} dsds`}
+        {`${title} dsdsdsdsds`}
       </Text>
       <Text
         style={[
